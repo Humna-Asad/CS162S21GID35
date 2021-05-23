10be2bc045f09223ed54f63dc02940f3dca4058e
HumnaAsad <81908805+Humna-Asad@users.noreply.github.com>
2021-05-23 21:37:41 +0500

    6th commit

--- BakeryManagement/BakeryManagement/Form1.Designer.cs
+++ BakeryManagement/BakeryManagement/Form1.Designer.cs
@@ -149,7 +149,7 @@ namespace BakeryManagement
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(696, 477);
+            this.ClientSize = new System.Drawing.Size(723, 477);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label7);
@@ -161,7 +161,7 @@ namespace BakeryManagement
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.SystemColors.Info;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "MainMenu";
             this.Load += new System.EventHandler(this.Form1_Load);
